3a41219dab2720bad401638909a0ad4d503107d5
‘ChenYangQi’ <yangqi.chen@govee.com>
2023-09-17 05:19:41 +0800

    feat: 加密Lua脚本

--- app/src/main/java/com/govee/lua/MainActivity.kt
+++ app/src/main/java/com/govee/lua/MainActivity.kt
@@ -6,7 +6,6 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.govee.pad.base.lua.LuaManager
-import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity_TAG"
@@ -14,7 +13,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnDataToJson: Button
     private lateinit var tvResult1: TextView
     private lateinit var tvResult2: TextView
-    private val filePath = "lua_test.lua"
+    private lateinit var mLuaManager: LuaManager
+
+    /**
+     * 通过AES加密的lua文件
+     */
+    private val filePath = "encode_lua_file.text"
+
+    /**
+     * 解密Lua文件的秘钥
+     */
+    private val decodeKey = "cyq123456"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +40,12 @@ class MainActivity : AppCompatActivity() {
         btnDataToJson.setOnClickListener {
             dataToJson()
         }
+        initLuaManager()
+    }
+
+    private fun initLuaManager() {
+        mLuaManager = LuaManager(this)
+        mLuaManager.setDataKey("cyq123456")
     }
 
     /**
@@ -38,13 +53,15 @@ class MainActivity : AppCompatActivity() {
      */
     private fun jsonToData() {
         val jsonCmd = "{\"cmd_type\":\"control\",\"power\":\"on\",\"brightness\":\"88\"}"
-        val result = LuaManager(this).encodeJsonToBytes(filePath, jsonCmd)
+        // json转16进制指令
+        val result = mLuaManager.encodeJsonToBytes(filePath, jsonCmd)
+
         if (!TextUtils.isEmpty(result)) {
             val sb = StringBuilder()
-            sb.append("input：\n")
+            sb.append("输入：\n")
                 .append(jsonCmd)
                 .append("\n\n")
-                .append("output：\n")
+                .append("输出：\n")
                 .append(result)
 
             tvResult1.text = sb.toString()
@@ -56,15 +73,62 @@ class MainActivity : AppCompatActivity() {
      */
     private fun dataToJson() {
         val hexCmd = "010158"
-        val result = LuaManager(this).decodeBytesToJson(filePath, hexCmd)
+        // 16进制指令转json
+        val result = mLuaManager.decodeBytesToJson(filePath, hexCmd)
+
         if (!TextUtils.isEmpty(result)) {
             val sb = StringBuilder()
-            sb.append("input：\n")
+            sb.append("输入：\n")
                 .append(hexCmd)
                 .append("\n\n")
-                .append("output：\n")
+                .append("输出：\n")
                 .append(result)
             tvResult2.text = sb.toString()
         }
     }
+
+    /*private fun testEncode() {
+        val str1 = AESCrypt.encrypt(readLuaStringFromAssets(filePath), "cyq123456")
+        Log.e("AESCrypt", str1)
+        val file = File(cacheDir, "encode_test.lua")
+        if (file.exists()) {
+            file.delete()
+        }
+        file.createNewFile()
+        val writer = BufferedWriter(FileWriter(file))
+        writer.write(str1)
+        writer.close()
+        Log.e("AESCrypt", "文件写入完成=${file.absolutePath}")
+
+        val str2 = AESCrypt.decrypt(str1, "cyq123456")
+        Log.e("AESCrypt", str2)
+    }
+
+    */
+    /**
+     * 读取getAssets下的Lua文件，并转字符串
+     *
+     * @param fileName:lua文件目录
+     *//*
+    private fun readLuaStringFromAssets(fileName: String): String? {
+        return try {
+            if (TextUtils.isEmpty(fileName)) {
+                return ""
+            }
+            val inputStream: InputStream = assets.open(fileName)
+            val inputReader = InputStreamReader(inputStream)
+            val bufReader = BufferedReader(inputReader)
+            var line: String?
+            val Result = StringBuilder()
+            while (bufReader.readLine().also { line = it } != null) {
+                Result.append(line).append("\n")
+            }
+            bufReader.close()
+            inputReader.close()
+            inputStream.close()
+            Result.toString()
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }*/
 }
